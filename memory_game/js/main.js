42ab72457a5c6fb7956649e174e86031f6831b3e
suhaibbinhimd <sbinhimd@hotmail.com>
2019-09-26 04:25:09 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,46 +1,53 @@
-var cards = [
-{
-	rank: "queen" ,
-	suit: "hearts" ,
-	cardImage: "images/queen-of-hearts.png" ,
-},
-{
-	rank: "queen" ,
-	suit: "diamonds" ,
-	cardImage: "images/queen-of-diamonds.png" ,
-},
-{
-	rank: "king" ,
-	suit: "hearts" ,
-	cardImage: "images/king-of-hearts.png" ,
-},
-{
-	rank: "king" ,
-	suit: "diamonds" ,
-	cardImage: "images/king-of-diamonds.png" ,
-}
+var cards = [{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+}, {
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+}, {
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+}, {
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+}];
 
-];
 var cardsInPlay = [];
+var flipCount = 0;
 
 function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("MATCH FOUND!");
+     
+    } else {
+      alert("SORRY, TRY AGAIN!");
 
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
-	} else {
-		console.log("Sorry, try again");
-	}
-}
+      
+    };
+  };
+};
 
-function flipCard(cardId) {
+function flipCard() {
 	
+var cardId = this.getAttribute('data-id');
+this.setAttribute('src',cards[cardId].cardImage);
 console.log("User Flipped " + cards[cardId].rank);
-console.log(cards[cardId].suit);
-console.log(cards[cardId].cardImage);
 cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
+};
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+   var cardElement = document.createElement('img');
+   cardElement.setAttribute('src','images/back.png');
+   cardElement.setAttribute('data-id',i)
+   cardElement.addEventListener('click',flipCard);
+   document.getElementById('game-board').appendChild(cardElement);
 }
-
-flipCard(0);
-flipCard(2);
-
+}
+createBoard();
